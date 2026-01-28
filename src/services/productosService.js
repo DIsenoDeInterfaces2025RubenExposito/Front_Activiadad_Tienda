8bc3d5e5bdf36072f9f2fa8e1d49cbe0bad807c7
Rebirzt <rubenexvi@gmail.com>
2026-01-28 17:16:07 +0100

    Arreglados errores

--- src/services/productosService.js
+++ src/services/productosService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL+"/productos";
 
 const mapProductoFromAPI = (producto) => ({
     id: producto._id,
